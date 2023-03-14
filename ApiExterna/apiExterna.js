bfe1d97ac7d8c2d378cfc5ef5e8c2cff59f03e34
juliop11 <julioperezobrero@hotmail.com>
2023-03-14 12:01:22 +0100

    pokemon

--- ApiExterna/apiExterna.js
+++ ApiExterna/apiExterna.js
@@ -24,3 +24,45 @@ async function getPokemon() {
         console.log(error);
     }
 }
+
+
+async function postPokemon() {
+
+    let pokemon = new Pokemon(document.getElementById("nombre").value)
+
+    let url = "https://pokeapi.co/api/v2/pokemon/";
+
+    let param = {
+
+        headers: { "Content-type": "application/json; charset= UTF-8" },
+        body: JSON.stringify(pokemon),
+        method: "POST"
+    }
+    fetch(url, param)
+        .then(function (data) {
+            return data.json()
+        })
+        .then(function (result) {
+            if (result.error)
+                showToast("ERROR: " + result.mensaje, "bg-danger")
+            else
+                showToast("Usuario creado correctamente", "bg-success")
+
+                console.log(result);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+}
+
+
+function showToast(mensaje,color){
+
+    document.getElementById("toastText").innerText=mensaje;
+    let toastElement = document.getElementById("toast")
+
+    toastElement.className = toastElement.className.replace("bg-warning").replace("bg-danger") + " " + color;
+
+    let toast = new bootstrap.toast(toastElement)
+    toast.show()
+}
